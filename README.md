32ad83d6616e8bd50841403b82303b5f9972f465
sebas <sebastian.1701821561@ucaldas.edu.co>
2025-02-21 11:19:41 -0500

    frontend y backend

---
+++ README.md
@@ -0,0 +1,8 @@
+# iniciar backend
+./mvnw clean install
+./mvnw spring-boot:run
+
+# iniciar frontend
+npm install
+cd my-app
+npm start
---
+++ demo/src/test/java/proymodpredictivoia/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package proymodpredictivoia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
